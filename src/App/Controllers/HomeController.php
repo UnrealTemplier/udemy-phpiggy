d31b3b1a515201e5d9bc594ad37028f53c66d18f
UnrealTemplier <jaimore.evseev@gmail.com>
2024-12-21 19:45:00 +0400

    Added wip TemplateEngine.

--- src/App/Controllers/HomeController.php
+++ src/App/Controllers/HomeController.php
@@ -4,8 +4,17 @@ declare(strict_types=1);
 
 namespace App\Controllers;
 
+use Framework\TemplateEngine;
+
 class HomeController
 {
+    private TemplateEngine $view;
+
+    public function __construct()
+    {
+        $this->view = new TemplateEngine();
+    }
+
     public function home()
     {
         echo "Home page";

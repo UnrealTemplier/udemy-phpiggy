d172848d25b39d1e358e47b634168a53d3fb51fb
UnrealTemplier <jaimore.evseev@gmail.com>
2024-12-26 10:04:03 +0400

    Add FlashMiddleware to inject validation errors from $_SESSION to register.php template, and flashing (erasing) them from the $_SESSION afterwards.

--- src/App/Config/Middleware.php
+++ src/App/Config/Middleware.php
@@ -4,12 +4,13 @@ declare(strict_types=1);
 
 namespace App\Config;
 
-use App\Middleware\{SessionMiddleware, TemplateDataMiddleware, ValidationExceptionMiddleware};
+use App\Middleware\{FlashMiddleware, SessionMiddleware, TemplateDataMiddleware, ValidationExceptionMiddleware};
 use Framework\App;
 
 function registerMiddleware(App $app)
 {
     $app->addMiddleware(TemplateDataMiddleware::class);
     $app->addMiddleware(ValidationExceptionMiddleware::class);
+    $app->addMiddleware(FlashMiddleware::class);
     $app->addMiddleware(SessionMiddleware::class);
 }

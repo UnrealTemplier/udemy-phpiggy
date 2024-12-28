769cfa66953e525dceca97581a68c33f2798ba31
UnrealTemplier <jaimore.evseev@gmail.com>
2024-12-28 16:37:16 +0400

    Using SQL file to execute query.

--- cli.php
+++ cli.php
@@ -17,21 +17,5 @@ $password = "";
 
 $db = new Database($driver, $config, $username, $password);
 
-try {
-    $db->connection->beginTransaction();
-
-    $db->connection->query("INSERT INTO products VALUES(99, 'Gloves');");
-    // $db->connection->query("DELETE FROM products WHERE id=99");
-
-    $where = "Gloves";
-    $query = "SELECT * FROM products WHERE name=:name";
-    $stmt = $db->connection->prepare($query);
-    $stmt->bindValue("name", $where, PDO::PARAM_STR);
-    $stmt->execute();
-    var_dump($stmt->fetchAll(PDO::FETCH_ASSOC));
-
-    $db->connection->commit();
-} catch (Exception $e) {
-    $db->connection->rollBack();
-    echo "Transation failed.";
-}
+$sqlFile = file_get_contents("./database.sql");
+$db->connection->query($sqlFile);

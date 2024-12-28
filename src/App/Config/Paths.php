03e94ab5398cfc2af784413123f7d33c5635b6c3
UnrealTemplier <jaimore.evseev@gmail.com>
2024-12-28 23:27:48 +0400

    Add ROOT const to Paths config.

--- src/App/Config/Paths.php
+++ src/App/Config/Paths.php
@@ -6,6 +6,7 @@ namespace App\Config;
 
 class Paths
 {
+    public const ROOT = __DIR__ . "/../../../";
     public const SOURCE = __DIR__ . "/../../";
     public const VIEWS = __DIR__ . "/../views";
 }

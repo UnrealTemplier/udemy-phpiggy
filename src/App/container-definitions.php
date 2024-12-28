97621af3e6c88587081178e15a0d1ce002ad96be
UnrealTemplier <jaimore.evseev@gmail.com>
2024-12-28 23:15:25 +0400

    Add Database class to container-definitions.

--- src/App/container-definitions.php
+++ src/App/container-definitions.php
@@ -4,9 +4,14 @@ declare(strict_types=1);
 
 use App\Config\Paths;
 use App\Services\ValidatorService;
-use Framework\TemplateEngine;
+use Framework\{TemplateEngine, Database};
 
 return [
     TemplateEngine::class => fn() => new TemplateEngine(Paths::VIEW),
     ValidatorService::class => fn() => new ValidatorService(),
+    Database::class => fn() => new Database("mysql", [
+        "host"      => "localhost",
+        "port"      => 3306,
+        "dbname"    => "phpiggy",
+    ], "root", ""),
 ];

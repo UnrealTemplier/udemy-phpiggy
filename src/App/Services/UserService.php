b71d9ce14dd487f39bbb9514319b08fd3729550b
UnrealTemplier <jaimore.evseev@gmail.com>
2024-12-29 17:26:34 +0400

    Fix duplicate email validation exception throwing.

--- src/App/Services/UserService.php
+++ src/App/Services/UserService.php
@@ -22,12 +22,15 @@ class UserService
      */
     public function checkEmailTaken(string $email)
     {
-        $emailCount = $this->db->query("SELECT COUNT(*) FROM users WHERE email=:email", [
-            "email" => $email,
-        ])->count();
+        $emailCount = $this->db->query(
+            "SELECT COUNT(*) FROM users WHERE email=:email",
+            [
+                "email" => $email,
+            ],
+        )->count();
 
         if ($emailCount > 0) {
-            throw new ValidationException(["email" => "Email taken."]);
+            throw new ValidationException(["email" => ["Email is already taken."]]);
         }
     }
 

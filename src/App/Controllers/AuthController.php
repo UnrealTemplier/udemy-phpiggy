b312c0bde5034c4084f695bbaecd9b6c208c6f30
UnrealTemplier <jaimore.evseev@gmail.com>
2024-12-29 16:59:28 +0400

    Add duplicate emails validation to UserService and AuthController.

--- src/App/Controllers/AuthController.php
+++ src/App/Controllers/AuthController.php
@@ -26,5 +26,7 @@ class AuthController
     public function register()
     {
         $this->formValidationService->validateRegister($_POST);
+        $this->userService->checkEmailTaken($_POST["email"]);
+        redirectTo("/");
     }
 }
--- src/App/Services/UserService.php
+++ src/App/Services/UserService.php
@@ -5,8 +5,29 @@ declare(strict_types=1);
 namespace App\Services;
 
 use Framework\Database;
+use Framework\Exceptions\ValidationException;
 
 class UserService
 {
     public function __construct(private Database $db) {}
+
+    /**
+     * Checks if provided email is already taken.
+     *
+     * @param string $email Email address to check.
+     *
+     * @return void
+     *
+     * @throws ValidationException Throws an exception if email is already taken.
+     */
+    public function checkEmailTaken(string $email)
+    {
+        $emailCount = $this->db->query("SELECT COUNT(*) FROM users WHERE email=:email", [
+            "email" => $email,
+        ])->count();
+
+        if ($emailCount > 0) {
+            throw new ValidationException(["email" => "Email taken."]);
+        }
+    }
 }

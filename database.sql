d73a1e277540c08ab97aadfd0d1422ff8ec5a6bf
UnrealTemplier <jaimore.evseev@gmail.com>
2025-02-19 12:32:16 +0400

    Add SQL code for creating transactions table.

--- database.sql
+++ database.sql
@@ -1,12 +1,26 @@
-CREATE TABLE IF NOT EXISTS users(
-    id BIGINT(20) UNSIGNED NOT NULL AUTO_INCREMENT,
-    email VARCHAR(255) NOT NULL,
-    password VARCHAR(255) NOT NULL,
-    age TINYINT(3) UNSIGNED NOT NULL,
-    country VARCHAR(255) NOT NULL,
-    social_media_url VARCHAR(255) NOT NULL,
-    created_at DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP(),
-    updated_at DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP(),
-    PRIMARY KEY(id),
-    UNIQUE KEY(email)
+CREATE TABLE IF NOT EXISTS users
+(
+    id               BIGINT(20) UNSIGNED NOT NULL AUTO_INCREMENT,
+    email            VARCHAR(255)        NOT NULL,
+    password         VARCHAR(255)        NOT NULL,
+    age              TINYINT(3) UNSIGNED NOT NULL,
+    country          VARCHAR(255)        NOT NULL,
+    social_media_url VARCHAR(255)        NOT NULL,
+    created_at       DATETIME            NOT NULL DEFAULT CURRENT_TIMESTAMP(),
+    updated_at       DATETIME            NOT NULL DEFAULT CURRENT_TIMESTAMP(),
+    PRIMARY KEY (id),
+    UNIQUE KEY (email)
+);
+
+CREATE TABLE IF NOT EXISTS transactions
+(
+    id          BIGINT(20) UNSIGNED NOT NULL AUTO_INCREMENT,
+    description VARCHAR(255)        NOT NULL,
+    amount      DECIMAL(10, 2)      NOT NULL,
+    date        DATETIME            NOT NULL,
+    created_at  DATETIME            NOT NULL DEFAULT CURRENT_TIMESTAMP(),
+    updated_at  DATETIME            NOT NULL DEFAULT CURRENT_TIMESTAMP(),
+    user_id     BIGINT(20) UNSIGNED NOT NULL,
+    PRIMARY KEY (id),
+    FOREIGN KEY (user_id) REFERENCES users (id)
 );
